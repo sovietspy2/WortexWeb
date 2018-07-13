22c7efbeaff6ba57d6312b833b67bbe434ca6ea3
Hang Barnabas <hang.barnabas@telekom.hu>
2018-07-13 14:44:09 +0200

    fixes

--- DAO.py
+++ DAO.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pymongo
 from pymongo import MongoClient
 import WortexLogger as w
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import flask
 import DAO as dao
 import PasswordHasher
