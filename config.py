b1cb0690f5b472f9044ff7391b51d3763b577296
Hang Barnabas <hang.barnabas@telekom.hu>
2018-07-17 14:07:18 +0200

    a

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 settings = {
     'SERVER':'PROD',
     'LOG_LOCATION':'wortex.log',
-    #'DB':'localhost',
-    'DB':'94.177.218.154',
+    'DB':'localhost',
+    #'DB':'94.177.218.154',
     'HOST':'0.0.0.0', # for debug
     'DEBUG': True,
     'PORT':8000,
