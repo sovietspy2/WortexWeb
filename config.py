fe628f0442180e8021a9e3b4f4f985b5f894eb42
Hang Barnabas <hang.barnabas@telekom.hu>
2018-07-17 14:06:53 +0200

    added config port and new dependencies

--- config.py
+++ config.py
@@ -2,8 +2,9 @@
 settings = {
     'SERVER':'PROD',
     'LOG_LOCATION':'wortex.log',
-    'DB':'localhost',
-    #'DB':'94.177.218.154',
+    #'DB':'localhost',
+    'DB':'94.177.218.154',
     'HOST':'0.0.0.0', # for debug
-    'DEBUG': False,
+    'DEBUG': True,
+    'PORT':8000,
 }
